8a1caffbd4cdcff489bb60c2c3559a72a12bf176
Pierrick Mériel-Bussy <Pierrick.meriel@gmail.com>
2021-04-18 22:57:25 +0200

    Initial commit

---
+++ app/src/main/java/pierrick/merielbussy/prisonfantasy/FirstActivity.kt
@@ -0,0 +1,15 @@
+package pierrick.merielbussy.prisonfantasy
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        findViewById<TextView>(R.id.fullTextView).text = genderSelected
+    }
+}
---
+++ app/src/main/java/pierrick/merielbussy/prisonfantasy/MainActivity.kt
@@ -0,0 +1,52 @@
+package pierrick.merielbussy.prisonfantasy
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.*
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings.Global.getString
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+var genderSelected = "testResultGender"
+var resultGender = ""
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.resultGender).text = getString(R.string.result_gender_f)
+
+
+                val selectGender = findViewById<Switch>(R.id.switchGenderButton)
+
+                selectGender.setOnCheckedChangeListener {selectGender, _ ->
+                    findViewById<TextView>(R.id.resultGender).text = if (selectGender.isChecked)
+                        getString(R.string.result_gender_m)
+                    else
+                        getString(R.string.result_gender_f)
+        }
+    }
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    fun onValidate(button:View) {
+        findViewById<Button>(R.id.buttonCreateID).setOnClickListener {
+
+            val selectGender = findViewById<Switch>(R.id.switchGenderButton)
+
+            if (selectGender.isChecked) {
+                genderSelected = getString(R.string.result_gender_m)
+                val intent = Intent(this, FirstActivity::class.java)
+                startActivity(intent)
+            } else {
+                genderSelected = getString(R.string.result_gender_f)
+                val intent = Intent(this, FirstActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
+}
+
